ab651d3bd939e5d0bce7b65395309706a151dd74
Yelisomah <yelisomahe@gmail.com>
2025-06-07 22:30:44 +0000

    Initial commmit with created project structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '../app/pages/home/home.component';
+import { CategoryComponent } from './pages/category/category.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    {
+        path: 'home',
+        component: HomeComponent,
+        title: 'Audiophile | Home'
+    },
+    {
+        path: 'category/:id',
+        component: CategoryComponent,
+        title: 'Audiophile | Category',
+    }
+
+
+
+
+];
+// loadComponent: () =>
+        // import('../app/home/home.component').then((c) => c.HomeComponent),
---
+++ src/app/shared/pipes/price-format/price-format.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-price-format',
+  imports: [],
+  templateUrl: './price-format.component.html',
+  styleUrl: './price-format.component.css'
+})
+export class PriceFormatComponent {
+
+}
